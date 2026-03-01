69ce3b5751be9b98e04d5e4d4a38e4adf9152078
Mehmet Ozdede <mehmethanifiozdede@icolud.com>
2026-03-01 18:37:30 +0300

    fix: center icon and text together in AnimatedGenerateButton

--- src/components/ui/animated-generate-button.tsx
+++ src/components/ui/animated-generate-button.tsx
@@ -70,52 +70,54 @@ export default function AnimatedGenerateButton({
           } as React.CSSProperties
         }
       >
-        <svg
-          className={clsx(
-            "ui-anim-btn-svg mr-2 flex-grow-0",
-            svgSizeClasses[size],
-            "fill-[color:var(--ui-anim-svg-fill)]",
-            "transition-[fill,filter,opacity] duration-400"
-          )}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
-          ></path>
-        </svg>
-        <div className="ui-anim-txt-wrapper relative flex items-center font-bold tracking-tight">
-          {/* Ghost text to drive container width */}
-          <span className="opacity-0 pointer-events-none whitespace-nowrap">
-            {labelIdle.length > labelActive.length ? labelIdle : labelActive}
-          </span>
+        <div className="flex items-center justify-center">
+          <svg
+            className={clsx(
+              "ui-anim-btn-svg mr-2 flex-grow-0",
+              svgSizeClasses[size],
+              "fill-[color:var(--ui-anim-svg-fill)]",
+              "transition-[fill,filter,opacity] duration-400"
+            )}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+            ></path>
+          </svg>
+          <div className="ui-anim-txt-wrapper relative flex items-center font-bold tracking-tight">
+            {/* Ghost text to drive container width */}
+            <span className="opacity-0 pointer-events-none whitespace-nowrap">
+              {labelIdle.length > labelActive.length ? labelIdle : labelActive}
+            </span>
 
-          <div
-            className={clsx(
-              "ui-anim-txt-1 absolute inset-0 flex items-center justify-start whitespace-nowrap",
-              generating ? "opacity-0" : "animate-[ui-appear_1s_ease-in-out_forwards]"
-            )}
-          >
-            {Array.from(labelIdle).map((ch, i) => (
-              <span key={i} className="ui-anim-letter inline-block">
-                {ch === " " ? "\u00A0" : ch}
-              </span>
-            ))}
-          </div>
-          <div
-            className={clsx(
-              "ui-anim-txt-2 absolute inset-0 flex items-center justify-start whitespace-nowrap",
-              generating ? "opacity-100" : "opacity-0"
-            )}
-          >
-            {Array.from(labelActive).map((ch, i) => (
-              <span key={i} className="ui-anim-letter inline-block">
-                {ch === " " ? "\u00A0" : ch}
-              </span>
-            ))}
+            <div
+              className={clsx(
+                "ui-anim-txt-1 absolute inset-0 flex items-center justify-start whitespace-nowrap",
+                generating ? "opacity-0" : "animate-[ui-appear_1s_ease-in-out_forwards]"
+              )}
+            >
+              {Array.from(labelIdle).map((ch, i) => (
+                <span key={i} className="ui-anim-letter inline-block">
+                  {ch === " " ? "\u00A0" : ch}
+                </span>
+              ))}
+            </div>
+            <div
+              className={clsx(
+                "ui-anim-txt-2 absolute inset-0 flex items-center justify-start whitespace-nowrap",
+                generating ? "opacity-100" : "opacity-0"
+              )}
+            >
+              {Array.from(labelActive).map((ch, i) => (
+                <span key={i} className="ui-anim-letter inline-block">
+                  {ch === " " ? "\u00A0" : ch}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
       </button>
