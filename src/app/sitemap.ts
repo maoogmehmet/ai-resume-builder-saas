c9a208f6a1eae0e40bc5d2e005615cc64fb1151d
maoogmehmet <memoozdede@gmail.com>
2026-03-06 00:22:41 +0300

    fix: correct sitemap return type

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next'
 
-export default function sitemap(): MetadataRoute.Robots {
+export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://novatypalcv.com'
 
     return [
